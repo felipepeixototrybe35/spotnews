7e00ca193d33ef196d78509f222d071a668aea5c
Felipe-33 <fepepiuerj@gmail.com>
2024-09-24 18:19:49 -0300

    Requisito 3

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import ValidationError
 
 
 class Category(models.Model):
@@ -16,3 +17,20 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateField(blank=False, null=False)
+    image = models.ImageField(upload_to='img/', blank=True, null=True)
+    categories = models.ManyToManyField('Category', blank=False)
+
+    # Validação para garantir que o título tenha mais de uma palavra
+    def clean(self):
+        if len(self.title.split()) < 2:
+            raise ValidationError('O título deve ter mais de uma palavra.')
+
+    def __str__(self):
+        return self.title
