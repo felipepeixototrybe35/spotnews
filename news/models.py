d1ea5c768d7479b06c6da77c7fff8febb1d566c6
Felipe-33 <fepepiuerj@gmail.com>
2024-09-24 21:32:48 -0300

    Corrigindo validação do title

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
+# import datetime
 from django.db import models
-from django.forms import ValidationError
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -19,18 +20,29 @@ class User(models.Model):
         return self.name
 
 
+def validate_title(value):
+    if len(value.split()) < 2:
+        raise ValidationError('O título deve conter pelo menos 2 palavras.')
+
+
 class News(models.Model):
-    title = models.CharField(max_length=200, blank=False, null=False)
+    title = models.CharField(
+        max_length=200,
+        blank=False,
+        null=False,
+        validators=[validate_title]  # Adiciona o validador personalizado
+    )
     content = models.TextField(blank=False, null=False)
     author = models.ForeignKey('User', on_delete=models.CASCADE)
     created_at = models.DateField(blank=False, null=False)
     image = models.ImageField(upload_to='img/', blank=True, null=True)
     categories = models.ManyToManyField('Category', blank=False)
 
-    # Validação para garantir que o título tenha mais de uma palavra
-    def clean(self):
-        if len(self.title.split()) < 2:
-            raise ValidationError('O título deve ter mais de uma palavra.')
-
     def __str__(self):
         return self.title
+
+    # def clean(self):
+    #     if len(self.title.split()) < 2:
+    #         raise ValidationError(
+    #             'O título deve conter pelo menos 2 palavras.'
+    #             )
