1b3caca70888c6fa5ae5a4f58ef87b2eac6de3ec
Felipe-33 <fepepiuerj@gmail.com>
2024-09-25 23:46:52 -0300

    Requisito 5 sem a alteração no home.html

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home-page'),
+    path('news/<int:id>/', views.news_details_view, name='news-details-page'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import News
 
 
 def home_view(request):
     news = News.objects.all()  # Obter todas as notícias do banco de dados
     return render(request, 'home.html', {'news': news})
+
+
+def news_details_view(request, id):
+    news_item = get_object_or_404(News, id=id)
+    return render(request, 'news_details.html', {'news_item': news_item})
+
